70a3f849efb2fd4490f0da633df433e76d801879
Raphael Lekies <raphael.lekies@it4s.eu>
2016-11-16 21:00:26 +0100

    create virtual environment to build the package

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# create all debian relevant files
-make-deb
+# create a virtual environment
+virtualenv virt-example
 
-chmod +x debian/rules
+# active virtual environment
+source ~/virt-example/bin/active
+
+pip install -U pip
+pip install -U setuptools
 
 # package all files
 dpkg-buildpackage -us -uc
