86cbab5be879a20b59862e6013c28649a9705c82
hack3d <hack3d.dev@googlemail.com>
2020-05-05 23:51:25 +0200

    Create automatically changelog on snapshot build

--- changelog.sh
+++ changelog.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Author:Andrey Nikishaev, IT4smart GmbH (Raphael Lekies)
+
+DIST=$1
+DIST_NO=$2
+
+
 git tag -l | sort -u -r | while read TAG ; do
     #echo
     if [ $NEXT ];then
-        echo "management-agent-it4smart (${NEXT#'v'}) jessie; urgency=low"
+        echo "management-agent-it4smart (${NEXT#'v'}~${DIST_NO}) ${DIST}; urgency=low"
     fi
     GIT_PAGER=cat git log --no-merges --format="  * %s%n" $TAG..$NEXT
 
@@ -14,6 +19,6 @@ git tag -l | sort -u -r | while read TAG ; do
 done
 FIRST=$(git tag -l | head -1)
 echo
-echo "management-agent-it4smart (${FIRST#'v'}) jessie; urgency=low"
+echo "management-agent-it4smart (${FIRST#'v'}~${DIST_NO}) ${DIST}; urgency=low"
 GIT_PAGER=cat git log --no-merges --format="  * %s%n" $FIRST
 echo " -- IT4smart GmbH <support@it4smart.eu> $(git log -n1 --no-merges --format='%aD' $FIRST)"
