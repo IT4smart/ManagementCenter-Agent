c42e65c7e9b75a00e67d5495bd7ef134fb384125
hack3d <hack3d.dev@googlemail.com>
2020-05-06 08:36:16 +0200

    Create changelog with commit hash for snapshots

--- changelog.sh
+++ changelog.sh
@@ -9,10 +9,16 @@ git tag -l | sort -u -r | while read TAG ; do
     #echo
     if [ $NEXT ];then
         echo "management-agent-it4smart (${NEXT#'v'}~${DIST_NO}) ${DIST}; urgency=low"
+    else
+        GIT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+        GIT_COMMIT=$(git log --pretty=format:'%h' -n 1)
+        echo "management-agent-it4smart (${GIT_TAG#'v'}+git-${GIT_COMMIT}~${DIST_NO}) ${DIST}; urgency=low"
     fi
     GIT_PAGER=cat git log --no-merges --format="  * %s%n" $TAG..$NEXT
 
     if [ $NEXT ];then
+        echo " -- IT4smart GmbH <support@it4smart.eu>  $(git log -n1 --no-merges --format='%aD' $NEXT)"
+    else
         echo " -- IT4smart GmbH <support@it4smart.eu>  $(git log -n1 --no-merges --format='%aD' $NEXT)"
     fi
     NEXT=$TAG
